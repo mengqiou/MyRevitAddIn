9b5d1852bfe62856fc7fa9a0b612979477681742
mengqiou <mengqiou92@gmail.com>
2024-02-09 14:43:45 +1100

    Add project files.

---
+++ AutoDim/GenDims.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.DB.Structure;
+
+namespace AutoDim
+{
+    [Transaction(TransactionMode.Manual)]
+    class GenDims : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData revit, ref string message, ElementSet elements)
+        {
+            // get all the columns element in current view
+            Document doc = revit.Application.ActiveUIDocument.Document;
+            View activeView = doc.ActiveView;
+            FilteredElementCollector collector = new FilteredElementCollector(doc, activeView.Id);
+            ElementCategoryFilter filter = new ElementCategoryFilter(BuiltInCategory.OST_StructuralColumns);
+            ICollection<Element> columns = collector.WherePasses(filter).WhereElementIsNotElementType().ToElements();
+
+
+            FilteredElementCollector collector2 = new FilteredElementCollector(doc, activeView.Id);
+            ElementCategoryFilter filter2 = new ElementCategoryFilter(BuiltInCategory.OST_Grids);
+            IList<Element> Grids = collector2.WherePasses(filter2).WhereElementIsNotElementType().ToElements();
+
+
+            // get all the horizontal grids
+            List<Element> hori = HorizontalGrids(Grids);
+            List<Element> vertical = VerticalGrids(Grids);
+
+            // get all the vertical grids
+
+
+            //TaskDialog dialog = new TaskDialog("Column infos");
+            //dialog.MainInstruction = "Element IDs of Columns in the View:";
+            foreach (Element column in columns)
+            {
+                XYZ p = (column.Location as LocationPoint).Point;
+                // how to creat a reference from the column center point?
+                // Create a reference to the column center
+                ReferencePoint referencePoint = doc.FamilyCreate.NewReferencePoint(p);
+                Reference colRef = new Reference(referencePoint);
+                
+
+                Grid hGrid = ClosestGrid(p, hori) as Grid;
+                Reference horiRef = new Reference(hGrid);
+                ReferenceArray horiRefs = new ReferenceArray();
+                horiRefs.Append(horiRef);
+                horiRefs.Append(colRef);
+                IntersectionResult result1 = (hGrid.Curve as Line).Project(p);
+                Line dimension1 = Line.CreateBound(p, result1.XYZPoint);
+
+                ReferenceArray vertiRefs = new ReferenceArray();
+                Grid vGrid = ClosestGrid(p, vertical) as Grid;
+                Reference vertiRef = new Reference(vGrid);
+                vertiRefs.Append(vertiRef);
+                vertiRefs.Append(colRef);
+                IntersectionResult result2 = (vGrid.Curve as Line).Project(p);
+                Line dimension2 = Line.CreateBound(p, result2.XYZPoint);
+                
+                using (Transaction transaction = new Transaction(doc, "new dimension"))
+                {
+                    transaction.Start();
+
+                    Dimension new1 = doc.Create.NewDimension(activeView, dimension1, horiRefs);
+                    Dimension new2 = doc.Create.NewDimension(activeView, dimension2, vertiRefs);
+                    transaction.Commit();
+                }
+
+            }
+
+            //dialog.Show();
+
+            return Result.Succeeded;
+        }
+
+        private List<Element> HorizontalGrids(IList<Element> grids)
+        {
+            List <Element> HorizontalGrids = new List<Element>();
+            foreach (Element grid in grids)
+            {
+                Grid g = grid as Grid;
+
+                Line gridLine = g.Curve as Line;
+                XYZ absDirection = new XYZ(Math.Abs(gridLine.Direction.X), Math.Abs(gridLine.Direction.Y), Math.Abs(gridLine.Direction.Z));
+                if (absDirection.IsAlmostEqualTo(XYZ.BasisX))
+                {
+                    HorizontalGrids.Add(grid);
+                }
+            }
+
+            return HorizontalGrids;
+        }
+
+        private List<Element> VerticalGrids(IList<Element> grids)
+        {
+            List<Element> HorizontalGrids = new List<Element>();
+            foreach (Element grid in grids)
+            {
+                Grid g = grid as Grid;
+
+                Line gridLine = g.Curve as Line;
+                XYZ absDirection = new XYZ(Math.Abs(gridLine.Direction.X), Math.Abs(gridLine.Direction.Y), Math.Abs(gridLine.Direction.Z));
+                if (absDirection.IsAlmostEqualTo(XYZ.BasisY))
+                {
+                    HorizontalGrids.Add(grid);
+                }
+            }
+
+            return HorizontalGrids;
+        }
+
+        private Element ClosestGrid(XYZ p, List<Element> grids)
+        {
+            double minDistance = double.MaxValue;
+            Element closestGrid = null;
+            foreach (Element g in grids)
+            {
+                Grid grid = g as Grid;
+
+                Line gridLine = grid.Curve as Line;
+
+                // Calculate the perpendicular distance from the point to the grid line
+                double distance = gridLine.Distance(p);
+                // Update closest grid if the current grid is closer
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    closestGrid = g;
+                }
+            }
+
+            return closestGrid;
+        }
+    }
+}
---
+++ AutoDim/PanelButton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Media.Imaging;
+using Autodesk.Revit.UI;
+
+namespace AutoDim
+{
+    public class PanelButton: IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+        {
+            // Add a new ribbon panel
+            RibbonPanel ribbonPanel1 = application.CreateRibbonPanel("NewRibbonPanel1");
+
+            // Add a new button
+            // When clicking this button Class CastOpening will be executed
+            PushButton pushButton = ribbonPanel1.AddItem(
+                       new PushButtonData("AutoDim", "AutoDim",
+                        @"C:\my projects\AutoDim\AutoDim\bin\Debug\AutoDim.dll",
+                        "AutoDim.GenDims")) as PushButton;
+
+            // Assign an image to the button
+            Uri uriImage = new Uri(@"C:\Program Files\Autodesk\Revit 2024\AddIns\HelloRevit\TestImg.png");
+            BitmapImage largeImage = new BitmapImage(uriImage);
+            pushButton.LargeImage = largeImage;
+
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+
+}
